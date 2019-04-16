403d98c4729d0a293578ce32a65b6fb212f3e654
Daniel Khaleb Batista <danielkhaleb@outlook.com>
2019-04-16 17:29:21 -0300

    add project

---
+++ main.ts
@@ -0,0 +1,46 @@
+import * as restify from 'restify';
+
+const server = restify.createServer({
+    name: 'ML-POC-api',
+    version: '1.0.0'
+})
+
+server.use(restify.plugins.queryParser())
+
+server.get('/info', [ 
+    (req, resp, next) => {
+        if(req.userAgent() && req.userAgent().includes('Mozilla/5.0')){
+            resp.status(400);
+            resp.json({message: 'errou'})
+            return next(false);
+        }
+        return next();
+    },
+    (req, resp, next) => {
+   //resp.status(400)
+    resp.json({
+     bwoser: req.userAgent(),
+     method: req.method,
+     query: req.query
+    })
+   return next() // callback terminou  | é possivel ter um array de de callback e o next nes caso vai para o próximo callback
+}
+
+])
+
+server.get('/info', (req, resp, next) => {
+    //resp.status(400)
+     resp.json({
+      bwoser: req.userAgent(),
+      method: req.method,
+      query: req.query
+     })
+    return next() // callback terminou  | é possivel ter um array de de callback e o next nes caso vai para o próximo callback
+ },
+ 
+ )
+ 
+
+server.listen(3000, ()=>{
+   console.log('Api is running at port 3000')
+});
